9f686e3d9741c20dc730d089a4016145275579ad
Michael Danylchuk <mn_danylchuk@yahoo.com>
2024-12-11 18:24:31 -0800

    Initial Commit

---
+++ Notepad++/Notepad__App.swift
@@ -0,0 +1,17 @@
+//
+//  Notepad__App.swift
+//  Notepad++
+//
+//  Created by Michael Danylchuk on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Notepad__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
